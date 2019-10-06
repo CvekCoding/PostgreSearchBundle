6156c93df3056fc06acaf63e3d56828555870122
Sergey Logachev <serg@baitek.org>
2019-10-06 13:30:11 +0300

    use tsrankcd

--- DQL/TsrankPlaintoFunction.php
+++ DQL/TsrankPlaintoFunction.php
@@ -25,7 +25,7 @@ class TsrankPlaintoFunction extends FunctionNode
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
         return 
-        	'ts_rank(' . $this->fieldName->dispatch($sqlWalker) . ', ' .
+        	'ts_rank_cd(' . $this->fieldName->dispatch($sqlWalker) . ', ' .
             ' plainto_tsquery(' . $this->queryString->dispatch($sqlWalker) . '))';
     }
 }
