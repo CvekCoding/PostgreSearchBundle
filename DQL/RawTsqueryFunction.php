6c2f2f2cd6a391a4e8b2af9bbef35a782a7d84e6
Sergey Logachev <serg@baitek.org>
2019-11-19 16:01:11 +0300

    Added raw tsquery

---
+++ DQL/RawTsqueryFunction.php
@@ -0,0 +1,44 @@
+<?php
+namespace Ddmaster\PostgreSearchBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * TsqueryFunction ::= "TSQUERY" "(" StringPrimary "," StringPrimary "[, " StringPrimary "])"
+ */
+class RawTsqueryFunction extends FunctionNode
+{
+    public $fieldName = null;
+    public $queryString = null;
+    public $configuration = null;
+    
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->fieldName = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->queryString = $parser->StringPrimary();
+        
+        $lexer = $parser->getLexer();
+        if ($lexer->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->configuration = $parser->StringPrimary();
+        }
+        
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        if ($this->configuration) {
+            $stmnt = $this->fieldName->dispatch($sqlWalker) .  " @@ " . $this->configuration->dispatch($sqlWalker) . ", " . $this->queryString->dispatch($sqlWalker) . "::tsquery";
+        }
+        else {
+            $stmnt = $this->fieldName->dispatch($sqlWalker) .  " @@ " . $this->queryString->dispatch($sqlWalker) . "::tsquery";
+        }
+        
+        return $stmnt;
+    }
+}
